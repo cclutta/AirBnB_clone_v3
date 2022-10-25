c9921924312e59a9657d3c2db0a55e3a733fd11d
cclutta <chelsea.lutta@gmail.com>
2022-10-25 20:19:16 +0300

    Update

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -24,17 +24,18 @@ def cities(id):
     """
     state = storage.get(State, id)
     if (state):
-        if request.method == 'GET':
-            return jsonify([c.to_dict() for c in state.cities])
-        elif request.method == 'POST':
+        if request.method == 'POST':
             kwargs = request.get_json()
             if not kwargs:
                 return {"error": "Not a JSON"}, 400
-                if "name" not in kwargs:
-                    return {"error": "Missing name"}, 400
-                new_city = City(state_id=id, **kwargs)
-                new_city.save()
-                return new_city.to_dict(), 201
+            if "name" not in kwargs:
+                return {"error": "Missing name"}, 400
+            new_city = City(state_id=id, **kwargs)
+            new_city.save()
+            return new_city.to_dict(), 201
+
+        elif request.method == 'GET':
+            return jsonify([c.to_dict() for c in state.cities])
     abort(404)
 
 
